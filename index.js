8fbbd4f67514ba465b3cbad1b8a4e2f54487cef2
stan123-lab <stmanolov@gmail.com>
2022-10-19 14:10:55 +0300

    8

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const functions = require('@google-cloud/functions-framework');
 
 functions.http('helloHttp', (req, res) => {
- res.send(`Hello ${req.query.name || req.body.name || 'Geeorgi'}!`);
+ res.send(`It is ${req.query.name || req.body.name || 'Wednesday'}!`);
 });
